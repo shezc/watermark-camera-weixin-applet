ae921990b69b772857885bf586829a36df7c179b
佘桢澄 <>
2021-12-06 16:47:16 +0800

    完成图层的拖动&引入阿里矢量图标

---
+++ miniprogram_npm/utrie/miniprogram_npm/base64-arraybuffer/index.js
@@ -0,0 +1,74 @@
+module.exports = (function() {
+var __MODS__ = {};
+var __DEFINE__ = function(modId, func, req) { var m = { exports: {}, _tempexports: {} }; __MODS__[modId] = { status: 0, func: func, req: req, m: m }; };
+var __REQUIRE__ = function(modId, source) { if(!__MODS__[modId]) return require(source); if(!__MODS__[modId].status) { var m = __MODS__[modId].m; m._exports = m._tempexports; var desp = Object.getOwnPropertyDescriptor(m, "exports"); if (desp && desp.configurable) Object.defineProperty(m, "exports", { set: function (val) { if(typeof val === "object" && val !== m._exports) { m._exports.__proto__ = val.__proto__; Object.keys(val).forEach(function (k) { m._exports[k] = val[k]; }); } m._tempexports = val }, get: function () { return m._tempexports; } }); __MODS__[modId].status = 1; __MODS__[modId].func(__MODS__[modId].req, m, m.exports); } return __MODS__[modId].m.exports; };
+var __REQUIRE_WILDCARD__ = function(obj) { if(obj && obj.__esModule) { return obj; } else { var newObj = {}; if(obj != null) { for(var k in obj) { if (Object.prototype.hasOwnProperty.call(obj, k)) newObj[k] = obj[k]; } } newObj.default = obj; return newObj; } };
+var __REQUIRE_DEFAULT__ = function(obj) { return obj && obj.__esModule ? obj.default : obj; };
+__DEFINE__(1638413361886, function(require, module, exports) {
+/*
+ * base64-arraybuffer 1.0.1 <https://github.com/niklasvh/base64-arraybuffer>
+ * Copyright (c) 2021 Niklas von Hertzen <https://hertzen.com>
+ * Released under MIT License
+ */
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global['base64-arraybuffer'] = {}));
+}(this, (function (exports) { 
+
+    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+    // Use a lookup table to find the index.
+    var lookup = typeof Uint8Array === 'undefined' ? [] : new Uint8Array(256);
+    for (var i = 0; i < chars.length; i++) {
+        lookup[chars.charCodeAt(i)] = i;
+    }
+    var encode = function (arraybuffer) {
+        var bytes = new Uint8Array(arraybuffer), i, len = bytes.length, base64 = '';
+        for (i = 0; i < len; i += 3) {
+            base64 += chars[bytes[i] >> 2];
+            base64 += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
+            base64 += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
+            base64 += chars[bytes[i + 2] & 63];
+        }
+        if (len % 3 === 2) {
+            base64 = base64.substring(0, base64.length - 1) + '=';
+        }
+        else if (len % 3 === 1) {
+            base64 = base64.substring(0, base64.length - 2) + '==';
+        }
+        return base64;
+    };
+    var decode = function (base64) {
+        var bufferLength = base64.length * 0.75, len = base64.length, i, p = 0, encoded1, encoded2, encoded3, encoded4;
+        if (base64[base64.length - 1] === '=') {
+            bufferLength--;
+            if (base64[base64.length - 2] === '=') {
+                bufferLength--;
+            }
+        }
+        var arraybuffer = new ArrayBuffer(bufferLength), bytes = new Uint8Array(arraybuffer);
+        for (i = 0; i < len; i += 4) {
+            encoded1 = lookup[base64.charCodeAt(i)];
+            encoded2 = lookup[base64.charCodeAt(i + 1)];
+            encoded3 = lookup[base64.charCodeAt(i + 2)];
+            encoded4 = lookup[base64.charCodeAt(i + 3)];
+            bytes[p++] = (encoded1 << 2) | (encoded2 >> 4);
+            bytes[p++] = ((encoded2 & 15) << 4) | (encoded3 >> 2);
+            bytes[p++] = ((encoded3 & 3) << 6) | (encoded4 & 63);
+        }
+        return arraybuffer;
+    };
+
+    exports.decode = decode;
+    exports.encode = encode;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=base64-arraybuffer.umd.js.map
+
+}, function(modId) {var map = {}; return __REQUIRE__(map[modId], modId); })
+return __REQUIRE__(1638413361886);
+})()
+//miniprogram-npm-outsideDeps=[]
+//# sourceMappingURL=index.js.map
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,50 @@
-// index.js
 // 获取应用实例
 const app = getApp()
-
 Page({
   data: {
     cameraMode: ['auto', 'on', 'off', 'torch'],
     cameraIndex: 0,
+    // 定义的canvas标签
+    wxml: `<view class="container">
+      <view class="item-box green" >
+        <text class="text">Yeah!</text>
+      </view>
+      </view>`,
+    wxmlStyle: {
+      container: {
+        width: 200,
+        height: 200,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        backgroundColor: '#ccc',
+        alignItems: 'center',
+      },
+      itemBox: {
+        width: 80,
+        height: 60,
+      },
+      green: {
+        backgroundColor: '#00ff00'
+      },
+      text: {
+        width: 80,
+        height: 60,
+        textAlign: 'center',
+        verticalAlign: 'middle',
+      },
+      img: {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+      }
+    },
+    pageX: 0,
+    pageY:0,
+    startX: 0,
+    startY:0,
+    endX: 0,
+    endY: 0,
+
     cameraObj: {
       auto: '自动',
       on: '打开',
@@ -13,12 +52,61 @@ Page({
       torch: '常亮'
     },
     deviceName: '后置',
+    widget: null,
+
 
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+  },
+
+  // div转canvas
+  onLoad() {
+    this.widget = this.selectComponent('.widget')
+  },
+  // 开始移动
+  touchStart (e) {
+    const { pageX, pageY } = e.changedTouches[0]
+    this.setData({
+      startX: pageX,
+      startY: pageY
+    })
+  },
+  // 结束移动(e) 
+  touchEnd ({target}) {
+    const {offsetLeft, offsetTop} = target
+    this.setData({
+      endX: offsetLeft,
+      endY: offsetTop
+    })
+  },
+  // 移动 
+  move(e) {
+    const { pageX, pageY } = e.changedTouches[0]
+    const currentPageX = pageX - this.data.startX + this.data.endX
+    const currentPageY = pageY - this.data.startY + this.data.endY
+    this.setData({
+      pageX: currentPageX < 0 ? 0 : currentPageX,
+      pageY: currentPageY < 0 ? 0 : currentPageY
+    })
+  },
+  renderToCanvas() {
+    const p1 = this.widget.renderToCanvas({ wxml:this.data.wxml, style: this.data.wxmlStyle })
+    p1.then((res) => {
+      this.container = res
+    })
+  },
+  extraImage() {
+    const p2 = this.widget.canvasToTempFilePath()
+    p2.then(res => {
+      this.setData({
+        src: res.tempFilePath,
+        width: this.container.layoutBox.width,
+        height: this.container.layoutBox.height
+      })
+    })
   },
   // 拍照
   record () {
@@ -51,20 +139,20 @@ Page({
     })
   },
   // 录像
-  takeVideo () {
-    wx.chooseVideo({
-      maxDuration:10,
-      success:function(res1){
-        app.startOperating("上传中")
-        // 这个就是最终拍摄视频的临时路径了
-        var tempFilePath=res1.tempFilePath;
-        console.log(tempFilePath)
-      },
-      fail:function(){
-        console.error("获取本地视频时出错");
-      }
-    })
-  },
+  // takeVideo () {
+  //   wx.chooseVideo({
+  //     maxDuration:10,
+  //     success:function(res1){
+  //       app.startOperating("上传中")
+  //       // 这个就是最终拍摄视频的临时路径了
+  //       var tempFilePath=res1.tempFilePath;
+  //       console.log(tempFilePath)
+  //     },
+  //     fail:function(){
+  //       console.error("获取本地视频时出错");
+  //     }
+  //   })
+  // },
   // 跳转到获取相册列表
   showPosition() {
     wx.navigateTo({
